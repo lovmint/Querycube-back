0f790e09ebfd39c6e9bd13d17779ace8a50faa37
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-07-24 01:37:44 +0900

    create project

---
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/QueryCubeApplication.java
@@ -0,0 +1,13 @@
+package likeLion.hackathon.team1.queryCube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryCubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryCubeApplication.class, args);
+	}
+
+}
---
+++ queryCube/src/test/java/likeLion/hackathon/team1/queryCube/QueryCubeApplicationTests.java
@@ -0,0 +1,13 @@
+package likeLion.hackathon.team1.queryCube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryCubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
