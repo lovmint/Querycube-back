a63e5438d5b86c430bf680c99be39ece4c36cdf6
sumina729 <114402652+sumina729@users.noreply.github.com>
2023-07-24 16:57:11 +0900

    add AnswerLikeRepository

---
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/repository/AnswerLikeRepository.java
@@ -0,0 +1,7 @@
+package likeLion.hackathon.team1.queryCube.domain.repository;
+
+import likeLion.hackathon.team1.queryCube.domain.entity.AnswerLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerLikeRepository extends JpaRepository<AnswerLike, Long>{
+}
