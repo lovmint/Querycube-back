caf479ce66895be426cfc4df2d9c51bdea02aec1
lovmint <sna135246@gmail.com>
2023-07-29 20:36:15 +0900

    멤버 Dto, Controller 코드

---
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/application/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package likeLion.hackathon.team1.queryCube.application.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GoogleLoginDto {
+
+    private String googleId;
+
+}
