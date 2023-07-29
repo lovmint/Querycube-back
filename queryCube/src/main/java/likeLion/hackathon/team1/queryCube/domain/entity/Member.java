cf8041f8d4303ecbd6b4d7afc5fed9b6657b467b
lovmint <sna135246@gmail.com>
2023-07-30 05:51:39 +0900

    Member 자체 페이지 내 회원가입,회원탈퇴, 구글계정 연동 간편로그인 기능 수정

--- queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/entity/Member.java
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/entity/Member.java
@@ -1,7 +1,6 @@
 package likeLion.hackathon.team1.queryCube.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -20,7 +19,6 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long member_id;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     private Region region_id;
 
@@ -28,15 +26,13 @@ public class Member {
     private String password; // Add this property
 
     private String name;
-
     private Integer reward_point;
-
-    private String googleId; // 구글 아이디 추가
+    private String googleId;
 
     @CreatedDate
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     @Column(updatable = false)
     private LocalDateTime create_date;
 
-    private boolean deleted = Boolean.FALSE;
+    private boolean deleted = false; // Use primitive boolean type instead of wrapper class
 }
--- queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/repository/MemberRepository.java
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/repository/MemberRepository.java
@@ -2,13 +2,11 @@ package likeLion.hackathon.team1.queryCube.domain.repository;
 
 import likeLion.hackathon.team1.queryCube.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByUsername(String username);
-
-    Optional<Member> findByGoogleId(String googleId); // 구글 아이디 멤버 조회
+    Optional<Member> findByGoogleId(String googleId);
 }
-
-
