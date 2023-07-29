d48cef8982473f698bc8e819c4ab2b9717a59c48
lovmint <sna135246@gmail.com>
2023-07-30 03:29:33 +0900

    Member unused import statement 수정1

--- queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/entity/Member.java
+++ queryCube/src/main/java/likeLion/hackathon/team1/queryCube/domain/entity/Member.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
